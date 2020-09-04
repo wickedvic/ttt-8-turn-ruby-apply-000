92788ea86f88cf00a4015237b94ece7c6a0f225a
Vivek Sanghvi <viksipart@gmail.com>
2020-09-04 19:17:30 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def position_taken?(board,index)
+  
+end
+
 def valid_move?(board, index)
   if index.between?(0,8)
     return true 
